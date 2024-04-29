92b5232fd2ba855f97a8e541ee15dea117b634f0
MajidFadlilla <168385513+MajidFadlilla@users.noreply.github.com>
2024-04-29 12:47:18 +0700

    Add files via upload

---
+++ persegi.js
@@ -0,0 +1,6 @@
+let Panjang = prompt(`masukkan panjang (cm)`)
+let lebar = prompt("masukkan lebar (cm)")
+const luasPersegi = Panjang * lebar
+
+alert(`dengan panjang sepanjang ${Panjang}cm dan lebar ${lebar}cm,
+ maka luas persegi tersebut adalah = ${luasPersegi}cm`)
